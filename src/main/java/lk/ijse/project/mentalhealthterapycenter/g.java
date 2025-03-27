1517b47b78348a687937ab372f69d9f030284727
Hirusha Silva <hirushasilva@Hirushas-MacBook-Air.local>
2025-03-27 10:58:53 +0530

    first commit

---
+++ src/main/java/lk/ijse/project/mentalhealthterapycenter/g.java
@@ -0,0 +1,16 @@
+package lk.ijse.project.mentalhealthterapycenter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class g extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.project.mentalhealthterapycenter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.project.mentalhealthterapycenter to javafx.fxml;
+    exports lk.ijse.project.mentalhealthterapycenter;
+}
