b5388f23d6cd7686a7a42ce89fb8eebc2f10a4ac
Hirusha Silva <hirushasilva@Hirushas-MacBook-Air.local>
2025-04-23 02:44:09 +0530

    complete therapy program controller

--- src/main/java/lk/ijse/project/mentalhealthterapycenter/controller/TherapyPrograms.java
+++ src/main/java/lk/ijse/project/mentalhealthterapycenter/controller/TherapyPrograms.java
@@ -1,15 +1,24 @@
 package lk.ijse.project.mentalhealthterapycenter.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import lk.ijse.project.mentalhealthterapycenter.bo.BOFactory;
+import lk.ijse.project.mentalhealthterapycenter.bo.BOType;
+import lk.ijse.project.mentalhealthterapycenter.bo.custom.TherapyProgramBO;
+import lk.ijse.project.mentalhealthterapycenter.dto.TherapyProgramDTO;
+import lk.ijse.project.mentalhealthterapycenter.dto.tm.TherapyProgramTm;
 
-public class TherapyPrograms {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class TherapyPrograms implements Initializable {
 
     @FXML
     private TextField ProgramDetails;
@@ -21,7 +30,7 @@ public class TherapyPrograms {
     private TextField ProgramName;
 
     @FXML
-    private TableView<?> Table;
+    private TableView<TherapyProgramTm> Table;
 
     @FXML
     private Button delete;
@@ -36,43 +45,133 @@ public class TherapyPrograms {
     private Button save;
 
     @FXML
-    private TableColumn<?, ?> tableFee;
+    private TableColumn<TherapyProgramTm,Double> tableFee;
 
     @FXML
-    private TableColumn<?, ?> tableIID;
+    private TableColumn<TherapyProgramTm, String> tableIID;
 
     @FXML
-    private TableColumn<?, ?> tableName;
+    private TableColumn<TherapyProgramTm,String> tableName;
 
     @FXML
-    private TableColumn<?, ?> tableProgramDetails;
+    private TableColumn<TherapyProgramTm,String> tableProgramDetails;
 
     @FXML
     private Button update;
 
-    @FXML
-    void deleteBtnAction(ActionEvent event) {
+    TherapyProgramBO tProgramBO = BOFactory.getInstance().getBO(BOType.THERAPY_PROGRAMS);
 
+    @FXML
+    void deleteBtnAction(ActionEvent event) throws Exception {
+        String programID = labelLoadID.getText();
+        boolean isDeleted = tProgramBO.deleteTProgram(programID);
+        if (isDeleted) {
+            refreshPage();
+            new Alert(Alert.AlertType.INFORMATION, "Programs Deleted").show();
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Programs Not Deleted").show();
+        }
     }
 
     @FXML
-    void resetBtnAction(ActionEvent event) {
-
+    void resetBtnAction(ActionEvent event) throws Exception {
+        refreshPage();
     }
 
     @FXML
-    void saveBtnAction(ActionEvent event) {
+    void saveBtnAction(ActionEvent event) throws Exception {
+        String therapyPID = labelLoadID.getText();
+        String therapyProgramName = ProgramName.getText();
+        String therapyProgramDetails = ProgramDetails.getText();
+        Double therapyProgramFee = Double.parseDouble(ProgramFee.getText());
 
+
+        TherapyProgramDTO therapyProgramDTO = new TherapyProgramDTO(
+                therapyPID,
+                therapyProgramName,
+                therapyProgramDetails,
+                therapyProgramFee
+        );
+        boolean isSaved = tProgramBO.saveTPrograms(therapyProgramDTO);
+
+        if (isSaved) {
+            new Alert(Alert.AlertType.INFORMATION, "Therapy Program Saved").show();
+            refreshPage();
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Therapy Program Not Saved").show();
+        }
     }
 
     @FXML
     void tableAction(MouseEvent event) {
-
+        TherapyProgramTm selectedPatient = Table.getSelectionModel().getSelectedItem();
+        if (selectedPatient != null) {
+            labelLoadID.setText(selectedPatient.getTherapyID());
+            ProgramName.setText(selectedPatient.getTherapyName());
+            ProgramDetails.setText(selectedPatient.getTherapyDescription());
+            ProgramFee.setText(String.valueOf(selectedPatient.getTherapyFee()));
+        }
     }
 
     @FXML
-    void updateBtnAction(ActionEvent event) {
+    void updateBtnAction(ActionEvent event) throws Exception {
+        String therapyPID = labelLoadID.getText();
+        String therapyProgramName = ProgramName.getText();
+        String therapyProgramDetails = ProgramDetails.getText();
+        Double therapyProgramFee = Double.parseDouble(ProgramFee.getText());
 
+        TherapyProgramDTO therapyProgramDTO = new TherapyProgramDTO(
+                therapyPID,
+                therapyProgramName,
+                therapyProgramDetails,
+                therapyProgramFee
+        );
+        boolean isUpdated = tProgramBO.updateTPrograms(therapyProgramDTO);
+
+        if (isUpdated) {
+            new Alert(Alert.AlertType.INFORMATION, "Therapy Programs updated successfully").show();
+            refreshPage();
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Therapy Programs updating Failed").show();
+        }
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableIID.setCellValueFactory(new PropertyValueFactory<>("therapyID"));
+        tableName.setCellValueFactory(new PropertyValueFactory<>("therapyName"));
+        tableProgramDetails.setCellValueFactory(new PropertyValueFactory<>("therapyDescription"));
+        tableFee.setCellValueFactory(new PropertyValueFactory<>("therapyFee"));
+
+        try{
+            refreshPage();
+        }catch(Exception e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to Load the Page", ButtonType.OK).show();
+        }
+    }
+
+    private void refreshPage() throws Exception {
+        loadTable();
+        labelLoadID.setText(tProgramBO.getNextProgramID());
+        ProgramName.clear();
+        ProgramDetails.clear();
+        ProgramFee.clear();
+    }
+
+    private void loadTable() throws Exception {
+        List<TherapyProgramDTO> programDtos =  tProgramBO.getALL();
+        ObservableList<TherapyProgramTm> programTMS = FXCollections.observableArrayList();
+        for (TherapyProgramDTO programDto : programDtos) {
+
+            TherapyProgramTm programTM = new TherapyProgramTm(
+                    programDto.getTherapyID(),
+                    programDto.getTherapyName(),
+                    programDto.getTherapyDescription(),
+                    programDto.getTherapyFee()
+            );
+            programTMS.add(programTM);
+        }
+        Table.setItems(programTMS);
+    }
 }
