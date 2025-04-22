259ae255dbcf5e28730391bef02f92606651fe95
Hirusha Silva <hirushasilva@Hirushas-MacBook-Air.local>
2025-04-23 02:24:55 +0530

    implement methods therapy program daoimpl

--- src/main/java/lk/ijse/project/mentalhealthterapycenter/dao/custom/impl/TherapyProgramDAOImpl.java
+++ src/main/java/lk/ijse/project/mentalhealthterapycenter/dao/custom/impl/TherapyProgramDAOImpl.java
@@ -1,8 +1,13 @@
 package lk.ijse.project.mentalhealthterapycenter.dao.custom.impl;
 
+import lk.ijse.project.mentalhealthterapycenter.config.FactoryConfiguration;
 import lk.ijse.project.mentalhealthterapycenter.dao.custom.TherapyProgramDAO;
 import lk.ijse.project.mentalhealthterapycenter.entity.TPrograms;
+import lk.ijse.project.mentalhealthterapycenter.exeception.DuplicateException;
+import lk.ijse.project.mentalhealthterapycenter.exeception.NotFoundException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -11,22 +16,59 @@ import java.util.Optional;
 public class TherapyProgramDAOImpl implements TherapyProgramDAO {
     @Override
     public boolean save(TPrograms tPrograms, Session session) throws SQLException {
-        return false;
+        try{
+            session.persist(tPrograms);
+            return true;
+        }catch (DuplicateException e){
+            throw new DuplicateException("Therapy already exists in therapistDAOImpl" + e.getMessage());
+        }catch (Exception e){
+            throw new SQLException("Therapy save failed in programDAOImpl" + e.getMessage());
+        }
     }
 
     @Override
     public boolean update(TPrograms tPrograms, Session session) throws SQLException, ClassNotFoundException {
-        return false;
+        try {
+            session.merge(tPrograms);
+            return true;
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<TPrograms> getAll() throws Exception {
-        return List.of();
+        Session session = FactoryConfiguration.getInstance().getSession();
+        try{
+            Query<TPrograms> query = session.createQuery("from TPrograms ", TPrograms.class);
+            return query.list();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }finally{
+            session.close();
+        }
     }
 
     @Override
     public boolean deleteByPk(String pk) throws SQLException, ClassNotFoundException {
-        return false;
+        Session session =FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            TPrograms tPrograms = session.get(TPrograms.class, pk);
+            if (tPrograms == null) {
+                throw new NotFoundException("Program not found");
+            }
+            session.remove(tPrograms);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
@@ -36,6 +78,17 @@ public class TherapyProgramDAOImpl implements TherapyProgramDAO {
 
     @Override
     public Optional<String> getLastPK() {
-        return Optional.empty();
+        Session session = FactoryConfiguration.getInstance().getSession();
+        try{
+            String lastPk = session
+                    .createQuery("SELECT t.id FROM TPrograms t ORDER BY t.id DESC", String.class)
+                    .setMaxResults(1)
+                    .uniqueResult();
+            return Optional.ofNullable(lastPk);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }finally {
+            session.close();
+        }
     }
 }
