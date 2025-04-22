3c9fd35b693b8a42c19af6947702aef0413d7dee
Hirusha Silva <hirushasilva@Hirushas-MacBook-Air.local>
2025-04-23 02:07:52 +0530

    create therapy program controller

--- src/main/java/lk/ijse/project/mentalhealthterapycenter/controller/TherapyPrograms.java
+++ src/main/java/lk/ijse/project/mentalhealthterapycenter/controller/TherapyPrograms.java
@@ -1,4 +1,78 @@
 package lk.ijse.project.mentalhealthterapycenter.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class TherapyPrograms {
+
+    @FXML
+    private TextField ProgramDetails;
+
+    @FXML
+    private TextField ProgramFee;
+
+    @FXML
+    private TextField ProgramName;
+
+    @FXML
+    private TableView<?> Table;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private Label labelLoadID;
+
+    @FXML
+    private Button reset;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private TableColumn<?, ?> tableFee;
+
+    @FXML
+    private TableColumn<?, ?> tableIID;
+
+    @FXML
+    private TableColumn<?, ?> tableName;
+
+    @FXML
+    private TableColumn<?, ?> tableProgramDetails;
+
+    @FXML
+    private Button update;
+
+    @FXML
+    void deleteBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tableAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void updateBtnAction(ActionEvent event) {
+
+    }
+
 }
